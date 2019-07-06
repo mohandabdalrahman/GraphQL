bc5710591fb10084d4401046c7f24443f8a6894e
mohand <mohand.abdalrahman94@yahoo.com>
2019-07-06 10:55:19 +0200

    test graphiql

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const expres=require('express');
 const graphqlHTTP=require('express-graphql');
+const schema=require('./schema/schema');
 const app=expres();
  //middleware
 app.use('/graphql', graphqlHTTP({
-    
+    schema:schema,
+    graphiql:true
 }))
 
 app.listen(4000,()=>{
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,12 @@
 const graphql=require("graphql");
-const {GraphQLObjectType,GraphQLString}=graphql;
-
+const {GraphQLObjectType,GraphQLString,GraphQLSchema}=graphql;
+const _=require('lodash');
+// dummy data
+var books=[
+    {name:'name of wind',id:1},
+    {name:'name of wind',id:2},
+    {name:'name of wind',id:3},
+]
 
 const BookType=new GraphQLObjectType({
     name:'Book',
@@ -9,4 +15,22 @@ const BookType=new GraphQLObjectType({
         name:{type:GraphQLString},
         genre:{type:GraphQLString},
     })
+})
+
+const RootQuery=new GraphQLObjectType({
+name:"RootQueryType",
+fields:{
+    book:{
+        type:BookType,
+        args:{id:{type:GraphQLString}},
+        resolve(parent,args){
+// code to get data from db
+return _.find(books,{id:args.id})
+        }
+    }
+}
+})   
+
+module.exports=new GraphQLSchema({
+    query: RootQuery
 })
